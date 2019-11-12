c3bc42315439727c02f71caf6b6b34d1a5b17e43
MaxwellOlliver <54994420+MaxwellOlliver@users.noreply.github.com>
2019-11-12 13:47:26 -0300

    Modificações no backend. APARENTEMENTE tá pronto.

--- src/models/Pendente.js
+++ src/models/Pendente.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 
 const pendentesSchema = new mongoose.Schema({
+    nomeCliente: String,
+    emailCliente: String,
+    cpfCliente: String,
+    telefoneCliente: String,
     nomeAnimal: String,
     tipoAnimal: String,
     racaAnimal: String,
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
+
 const SessionController = require("./controllers/sessionController");
 const PendenteController = require("./controllers/pendenteController");
+const AnimalController = require("./controllers/animalController");
+const LoginController = require("./controllers/loginController");
 
 const routes = express.Router();
 
-routes.post("/sessions", SessionController.store);
+routes.post("/login", LoginController.store);
+
+routes.post("/user", SessionController.store);
+
+routes.post("/user/animal", AnimalController.store);
 
 routes.post("/schedules", PendenteController.store);
 
